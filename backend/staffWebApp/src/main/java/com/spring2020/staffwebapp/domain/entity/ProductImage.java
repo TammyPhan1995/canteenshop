044a78c718f712ee986febcff07dffd87dccf053
Tram <hoang.tram2908@gmail.com>
2020-10-08 12:57:05 +0700

    merge from jinisminho's master

---
+++ backend/staffWebApp/src/main/java/com/spring2020/staffwebapp/domain/entity/ProductImage.java
@@ -0,0 +1,25 @@
+package com.spring2020.staffwebapp.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Product_Image")
+public class ProductImage
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String imgUrl;
+
+}
